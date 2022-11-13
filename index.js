bd9cdc10129161daf5459161c366fa146caf24a3
bjoern mergemann <hello@bjoernmergemann.de>
2022-11-13 12:40:15 +0100

    remove head data

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const template = (data) => (
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
   <body>
     ${data}
@@ -22,7 +19,6 @@ const template = (data) => (
 
 app.get('/', async (req, res) => {
   const data = await fs.readFile("./textfile.txt", "utf-8"); 
-  console.log(data);
   res.send(template(data))
 });
 
